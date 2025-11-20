994d411ddf3dc45df145c0019103c70ad80cc033
onsqcorp <onsqcorp@gmail.com>
2025-11-20 14:56:46 +0900

    Bump up dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,23 +2,20 @@
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.scalaAndroid)
-    id("org.barfuin.gradle.taskinfo") version "2.2.0"
 }
 // Possible choices of scala versions are
-// 2.11.x
-// 2.12.7 or earlier
 // 2.13.7 or later
 // 3.3.3 or later
-scala.scalaVersion = "3.7.0-RC2"
+scala.scalaVersion = "3.7.4"
 
 android {
     namespace = "com.soundcorset.helloscala"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.soundcorset.helloscala"
         minSdk = 28
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
